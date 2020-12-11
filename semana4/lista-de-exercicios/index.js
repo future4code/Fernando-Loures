ad2ab3080cec42c6e5f77c46ded737ef6d00495b
Fernando <fernando.loures@hotmail.com>
2020-12-11 18:05:12 -0300

    Exercicios de Funções de array - 3 - a e b.

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -310,32 +310,78 @@
 // Exercícios de funções de array
 // Exercício2
 
-const array = [1, 2, 3, 4, 5, 6]
-let novoArray =[]
-let arrayVezesDois = array.forEach((numero,index,array) =>{
-    numero = 2 * numero
-    novoArray.push(numero)
-})
-console.log(novoArray)
+// const array = [1, 2, 3, 4, 5, 6]
+// let novoArray =[]
+// let arrayVezesDois = array.forEach((numero,index,array) =>{
+//     numero = 2 * numero
+//     novoArray.push(numero)
+// })
+// console.log(novoArray)
 
-let novoArray2 =[]
-let arrayVezesTres = array.forEach((numero,index,array) =>{
-    numero = 3 * numero + ""
-    novoArray2.push((numero))
-})
-console.log(novoArray2)
+// let novoArray2 =[]
+// let arrayVezesTres = array.forEach((numero,index,array) =>{
+//     numero = 3 * numero + ""
+//     novoArray2.push((numero))
+// })
+// console.log(novoArray2)
 
-let novoArray3 =[]
-let arrayParImpar = array.forEach((numero,index,array) =>{
-    if (numero % 2 !==0) {
-        numero = numero + " é ímpar"  
-    } else{
-        numero = numero + " é par"
-    }
-    novoArray3.push((numero))
-})
+// let novoArray3 =[]
+// let arrayParImpar = array.forEach((numero,index,array) =>{
+//     if (numero % 2 !==0) {
+//         numero = numero + " é ímpar"  
+//     } else{
+//         numero = numero + " é par"
+//     }
+//     novoArray3.push((numero))
+// })
 
-console.log(novoArray3)
+// console.log(novoArray3)
+
+
+// Exercícios de funções de array
+// Exercício 3
+
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+
+// let permissaoPessoas = pessoas.filter((pessoa, index,array) =>{
+//     if ((pessoa.altura >= 1.5) && (pessoa.idade >= 14) && (pessoa.idade <= 60) ){
+//         return true
+//     }
+// })
+// console.log(permissaoPessoas)
+
+// let semPermissaoPessoas = pessoas.filter((pessoa, index,array) =>{
+//     if (!((pessoa.altura >= 1.5) && (pessoa.idade >= 14) && (pessoa.idade <= 60) )){
+//         return true
+//     }
+// })
+// console.log(semPermissaoPessoas)
+
+// let semPermissaoPessoas2 = pessoas.filter((pessoa, index,array) =>{
+//     if ((pessoa.altura < 1.5) || ((pessoa.idade < 14) || (pessoa.idade > 60)) ){
+//         return true
+//     }
+// })
+
+// console.log(semPermissaoPessoas2)
+
+
+
+
+
+
+
+
+
+
 
 
 
