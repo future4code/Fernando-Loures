7f6f0f10f6e1d7c16d8743f7cddf7623aa5100df
Fernando <fernando.loures@hotmail.com>
2020-12-18 10:07:22 -0300

    desafio1 - transformar a formatação do post em styled-component

--- semana5/insta4 2/src/components/Post/Post.js
+++ semana5/insta4 2/src/components/Post/Post.js
@@ -1,17 +1,62 @@
 import React from 'react'
 import './Post.css'
-
-import {IconeComContador} from '../IconeComContador/IconeComContador'
+import styled from 'styled-components'
+import { IconeComContador } from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
-import {SecaoCompartilhar} from '../secaoCompartilhar/SecaoCompartilhar'
+import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
+import { SecaoCompartilhar } from '../secaoCompartilhar/SecaoCompartilhar'
 
 import iconeSalvoBranco from '../../img/save-white.png'
 import iconeSalvoPreto from '../../img/save-black.svg'
 import iconeCompartilhar from '../../img/compartilhar.png'
+
+const PostContainer = styled.div` 
+  /* border: 1px solid gray; */
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`  
+height: 40px;
+display: flex;
+align-items: center;
+padding-left: 10px;
+`
+const UserPhoto = styled.img`
+height: 30px;
+width: 30px;
+margin-right: 10px;
+border-radius: 50%;`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+
+  img{
+    max-width: 20px;
+    /* max-height: 20px; */
+  }
+`
+const PostPhoto = styled.img `
+width: 100%;`
+  
+
+
+
+
+
+
+
+
+
+
+
 
 class Post extends React.Component {
   state = {
@@ -24,7 +69,7 @@ class Post extends React.Component {
   }
 
   onClickCompartilhar = () => {
-    this.setState({  
+    this.setState({
       compartilhar: !this.state.compartilhar
     })
   }
@@ -38,10 +83,10 @@ class Post extends React.Component {
 
   onClickCurtida = () => {
     console.log('Curtiu!')
-    if(!this.state.curtido){
-      this.state.numeroCurtidas +=1
-    }else{
-      this.state.numeroCurtidas -=1
+    if (!this.state.curtido) {
+      this.state.numeroCurtidas += 1
+    } else {
+      this.state.numeroCurtidas -= 1
     }
     this.setState({
       curtido: !this.state.curtido,
@@ -65,42 +110,42 @@ class Post extends React.Component {
   render() {
 
     let iconeSalvo
-    if (this.state.salvo){
+    if (this.state.salvo) {
       iconeSalvo = iconeSalvoPreto
-    }else{
+    } else {
       iconeSalvo = iconeSalvoBranco
     }
 
 
     let iconeCurtida
 
-    if(this.state.curtido) {
+    if (this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
     }
 
     let componenteCompartilhar
-    
-    if (this.state.compartilhar){
+
+    if (this.state.compartilhar) {
       componenteCompartilhar = <SecaoCompartilhar />
     }
 
     let componenteComentario
 
-    if(this.state.comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    if (this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
     }
 
-    return <div className={'post-container'}>
-      <div className={'post-header'}>
-        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+    return < PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'} />
         <p>{this.props.nomeUsuario}</p>
-      </div>
+      </PostHeader>
 
-      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'} />
 
-      <div className={'post-footer'}>
+      <PostFooter>
         <IconeComContador
           icone={iconeCurtida}
           onClickIcone={this.onClickCurtida}
@@ -122,10 +167,10 @@ class Post extends React.Component {
           onClickIcone={this.onClickCompartilhar}
           valorContador={""}
         />
-      </div>
+      </PostFooter>
       {componenteComentario}
       {componenteCompartilhar}
-    </div>
+    </ PostContainer>
   }
 }
 
